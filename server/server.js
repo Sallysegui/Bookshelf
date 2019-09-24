e4f3b86d135743f3c79acf30be4f14ef4683374b
Sally <sallysegui10@gmail.com>
2019-09-24 15:41:04 +0100

    changes to the server commit

--- server/server.js
+++ server/server.js
@@ -19,6 +19,9 @@ const { auth} = require('./middleware/auth')
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
+app.use(express.static('client/build'))
+
 // GET //
 
 app.get('/api/auth',auth,(req,res)=>{
@@ -174,6 +177,13 @@ app.delete('/api/delete_book',(req,res)=>{
     })
 })
 
+
+if(process.env.NODE_ENV === 'production'){
+   const path = require('path') 
+   app.get('/*',(req,res)=>{
+       res.sendfile(path.resolve(__dirname),'../client','build','index.html')
+   })
+}
 
 
 const port = process.env.PORT || 3001;
